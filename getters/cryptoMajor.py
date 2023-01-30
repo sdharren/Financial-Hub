84bfc73c1fdbfa3965fd72aaf45bb14410f01d44
Abhishek Rao Chimbili <abhishekraochimbili@gmail.com>
2023-01-30 15:37:54 +0000

    now working

--- getters/cryptoMajor.py
+++ getters/cryptoMajor.py
@@ -8,55 +8,55 @@ import json
 import requests as re
 
 # KEY is BTC/ETH and Value is List of addresses
-ADDRESSES = {"btc" : [], 
-             "eth" : []}
+ADDRESSES = {"btc" : ["34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo", "16ftSEQ4ctQFDtVZiUBusQUjRrGhM3JYwe"], 
+             "eth" : ["0x6090a6e47849629b7245dfa1ca21d94cd15878ef", ]}
 
 # All return values in JSON form
 # BTC Docs available @ https://www.blockcypher.com/dev/bitcoin/?python#address-endpoint
 # ETH Docs available @ https://www.blockcypher.com/dev/ethereum/#address-balance-endpoint
 class getAddressData:
     def BTC_all(addr):
-        return json.loads(get_address_full(address=addr, confirmations=3))
+        return get_address_full(address=addr, confirmations=3)
 
     def ETH_all(addr):
         command = "https://api.blockcypher.com/v1/eth/main/addrs/" + str(addr)
-        return re.get(command).json()
+        return re.get(command)
 
 
 def toBase(amount, type):
     if(type == "btc"):
-            amount = amount/(10^8)
+            amount = amount/(10**8)
     elif(type == "eth"):
-        amount = amount/(10^(18))
+        amount = amount/(10**(18))
 
     return amount
 
 
 class getUsable:
     def getAddress(data):
-        return (data["address"])
+        return (data[0].get("address"))
 
     def getBalance(data, type):
-        return toBase((data["final_balance"]), type)
+        return toBase((data[0].get("final_balance")), type)
     
     def getNoTx(data):
-        return (data["n_tx"])
+        return (data[0].get("n_tx"))
 
     def getTotalReceived(data, type):
-        return toBase((data["total_received"]), type)
+        return toBase((data[0].get("total_received")), type)
 
     def getTotalSent(data, type):
-        return toBase((data["total_sent"]), type)
+        return toBase((data[0].get("total_sent")), type)
 
     def getTxs(data, type):
         if(type == "btc"):
-            value = data["txs"]
+            value = data[0].get("txs")
         elif(type == "eth"):
-            value = data["txrefs"]
+            value = data[0].get("txrefs")
         
         return value
 
-    
+# On initialisation collect data for all addresses listed
 if __name__ == '__main__':
     data = [] # 2D Array where index 0 is actual data and index 1 is type (of coin)
 
@@ -72,3 +72,5 @@ if __name__ == '__main__':
         for addr in ethAddresses:
             value = [getAddressData.ETH_all(addr), "eth"]
             data.append(value)
+
+    print(getUsable.getBalance((data[0]), "btc"))
