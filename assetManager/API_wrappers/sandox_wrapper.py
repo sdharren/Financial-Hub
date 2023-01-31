10d74d805925387d772edbbe5fa7c89ebb2546b9
Pavan-Rana <pavan.rana@kcl.ac.uk>
2023-01-31 20:44:49 +0000

    add plaid_wrapper superclass

---
+++ assetManager/API_wrappers/sandox_wrapper.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import plaid
+from plaid.api import plaid_api
+from plaid.model.link_token_create_request import LinkTokenCreateRequest
+from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.model.products import Products
+from plaid.model.country_code import CountryCode
+from plaid.model.link_token_account_filters import LinkTokenAccountFilters
+from plaid.model.depository_filter import DepositoryFilter
+from plaid.model.depository_account_subtypes import DepositoryAccountSubtypes
+from plaid.model.depository_account_subtype import DepositoryAccountSubtype
+from plaid.model.sandbox_public_token_create_request import SandboxPublicTokenCreateRequest
+from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
+from plaid.model.accounts_get_request import AccountsGetRequest
+from assetManager.API_wrappers.investments import Investments
+from plaid_wrapper import PlaidWrapper
+
+class SandboxWrapper(PlaidWrapper):
+    def get_access_token():
