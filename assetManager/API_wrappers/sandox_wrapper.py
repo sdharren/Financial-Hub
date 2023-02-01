4735d464bf53c9bcf25e44b267f932e276a06e43
Pavan-Rana <pavan.rana@kcl.ac.uk>
2023-02-01 17:39:39 +0000

    Implement the sandbox_wrapper class

--- assetManager/API_wrappers/sandox_wrapper.py
+++ assetManager/API_wrappers/sandox_wrapper.py
@@ -17,4 +17,35 @@ from assetManager.API_wrappers.investments import Investments
 from plaid_wrapper import PlaidWrapper
 
 class SandboxWrapper(PlaidWrapper):
-    def get_access_token():
+    def __init__(self):
+        super().__init__()
+        self.SANDBOX_KEY = '3c1540e977fb113fe9bdbb12bf61fd'
+        self.PUBLIC_TOKEN = None
+
+        configuration = plaid.Configuration(
+        host=plaid.Environment.Development,
+        api_key={
+            'clientId': self.CLIENT_ID,
+            'secret': self.SANDBOX_KEY,
+            }
+        )
+
+        api_client = plaid.ApiClient(configuration)
+        self.client = plaid_api.PlaidApi(api_client)
+
+    def get_access_token(self):
+        return self.ACCESS_TOKEN
+
+    def get_item_id(self):
+        return self.ITEM_ID
+
+    def get_link_token(self):
+        return self.LINK_TOKEN
+
+    def create_public_token():
+        REVOLUT_ID = 'ins_115642' # revolut's ID
+        public_token_request = SandboxPublicTokenCreateRequest(
+            institution_id=REVOLUT_ID,
+            initial_products=[Products('transactions')]
+        )
+        return client.sandbox_public_token_create(public_token_request)
