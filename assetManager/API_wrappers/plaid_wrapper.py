7347d39dc42952f3b45599dcc6c4cd9787b21ae0
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2023-02-13 17:58:47 +0000

    Add tests for retrieve_access_token method

--- assetManager/API_wrappers/plaid_wrapper.py
+++ assetManager/API_wrappers/plaid_wrapper.py
@@ -101,12 +101,14 @@ class PlaidWrapper():
             return 'DEBIT'
 
     def _retrieve_access_token(self, user, product):
-        account = AccountType.objects.get(user = user, account_asset_type = self._transform_product_to_enum_value(product))
-        if account is not None:
-            self.access_token = account.access_token
-            return self.access_token
-        else:
+        try:
+            account = AccountType.objects.get(user = user, account_asset_type = self._transform_product_to_enum_value(product))
+        except AccountType.DoesNotExist:
             raise PublicTokenNotExchanged
+        else:
+            self.ACCESS_TOKEN = account.access_token
+        
+            
         
 
         
--- assetManager/tests/API_wrappers/test_plaid_wrapper.py
+++ assetManager/tests/API_wrappers/test_plaid_wrapper.py
@@ -1,2 +1,25 @@
 from django.test import TestCase
+from assetManager.models import User
 from assetManager.API_wrappers.plaid_wrapper import PlaidWrapper, PublicTokenNotExchanged
+
+class PlaidWrapperTestCase(TestCase):
+    fixtures = [
+        'assetManager/tests/fixtures/users.json',
+        'assetManager/tests/fixtures/account_types.json'
+        ]
+
+    def setUp(self):
+        self.wrapper = PlaidWrapper()
+
+    def test_cannot_retrieve_non_existent_access_token(self):
+        user = User.objects.get(email='johndoe@example.org')
+        product = 'investments'
+        with self.assertRaises(PublicTokenNotExchanged):
+            self.wrapper._retrieve_access_token(user, product)
+
+    def test_can_retrieve_existent_access_token(self):
+        user = User.objects.get(email='johndoe@example.org')
+        product = 'transactions'
+        self.wrapper._retrieve_access_token(user, product)
+        self.assertEqual(self.wrapper.get_access_token(), 'access-development-8ab976e6-64bc-4b38-98f7-731e7a349970')
+
