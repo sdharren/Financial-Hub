a640670fd1496a7846e56ebdabc2233af132ca5d
Pavan-Rana <pavan.rana@kcl.ac.uk>
2023-02-15 20:04:59 +0000

    add transactionInsight class and test class

---
+++ assetManager/transactionInsight/bank_transaction_insight.py
@@ -0,0 +1,6 @@
+class CategoriseTransactions():
+    def __init__(self,transaction_history):
+        self.transaction_history = transaction_history
+
+    def getTransactionHistory(self):
+        return self.transaction_history
