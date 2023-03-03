fbd18fa9de90c02241d7c4b5797300083abdb4cf
sdharren <sdharren@yahoo.co.uk>
2023-03-03 23:27:04 +0000

    customised token claims to include user email

--- assetManager/api/urls.py
+++ assetManager/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from .views import MyTokenObtainPairView
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
     path('', views.getRoutes),
-    path('token/', TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name = 'token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh')
 ]
--- assetManager/api/views.py
+++ assetManager/api/views.py
@@ -2,6 +2,21 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['email'] = user.email
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
