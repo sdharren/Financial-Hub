7165824898bc967f5859c13dd86e24d4f5171517
sdharren <sdharren@yahoo.co.uk>
2023-03-03 22:57:13 +0000

    installed simplejwt, added REST view for api folder

--- assetManager/api/views.py
+++ assetManager/api/views.py
@@ -1,8 +1,11 @@
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         '/api/token',
         '/api/token/refresh'
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
