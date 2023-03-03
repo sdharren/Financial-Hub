367c5665196fb43d8fbfdbbe9ac7942f03f8f4b2
sdharren <sdharren@yahoo.co.uk>
2023-03-03 23:09:24 +0000

    added simple JWT authentication

--- assetManager/api/urls.py
+++ assetManager/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
 urlpatterns = [
-    path('', views.getRoutes)
+    path('', views.getRoutes),
+    path('token/', TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh')
 ]
