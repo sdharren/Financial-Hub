d75c186a1eb2abacea94d14037270027cee89837
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2023-03-05 18:43:47 +0000

    Add comment to-dos

--- react-app/src/pages/Link.js
+++ react-app/src/pages/Link.js
@@ -7,17 +7,19 @@ function Link({ linkToken }) {
     let {authTokens, logoutUser} = useContext(AuthContext);
 
     async function onSuccess(public_token, metadata) {
-            // exchange public token
-            fetch('http://127.0.0.1:8000/api/exchange_public_token/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization':'Bearer ' + String(authTokens.access),
-            },
-            body: {
-                public_token,
-            },
-            });
+        // exchange public token
+        fetch('http://127.0.0.1:8000/api/exchange_public_token/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization':'Bearer ' + String(authTokens.access),
+        },
+        body: {
+            public_token,
+        },
+        });
+        // at this point the token is exchanged and the access token is saved
+        // need to now redirect/ tell parent component to render next thing
     }
 
     const config = {
