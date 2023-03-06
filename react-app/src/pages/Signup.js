3f82863ccf5c1a1cd605d39cab308c39106b41e2
sdharren <sdharren@yahoo.co.uk>
2023-03-06 03:15:34 +0000

    created signup form and connected SignupUser function to api call

--- react-app/src/pages/Signup.js
+++ react-app/src/pages/Signup.js
@@ -1,3 +1,39 @@
-export default function Signup() {
-    return <h1>This is the Sign Up Page</h1>
+import React, {useContext} from 'react';
+import AuthContext from '../context/AuthContext';
+
+const Signup = () => {
+
+    let SignupUser = async (e) => {
+        e.preventDefault();
+        let response = await fetch('http://127.0.0.1:8000/api/signup/', {
+            method : 'POST',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({
+                'email' : e.target.email.value,
+                'first_name' : e.target.first_name.value,
+                'last_name' : e.target.last_name.value,
+                'password' : e.target.password.value
+            })
+        })
+
+        let data = await response.json()
+        console.log(data)
+
+    }
+    
+    return (
+        <div>
+            <form onSubmit={SignupUser}>
+            <input type = "text" name = "email" placeholder='Enter email:' />
+            <input type = "text" name = "first_name" placeholder='Enter first name:' />
+            <input type = "text" name = "last_name" placeholder='Enter last name:' />
+            <input type = "password" name='password' placeholder='Enter password'/>
+            <input type = "submit"/>
+            </form>
+        </div>
+    )
 }
+
+export default Signup
