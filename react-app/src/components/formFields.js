1be271b037214b1e2f52aacec8ffc768c007eae3
sdharren <sdharren@yahoo.co.uk>
2023-03-13 04:04:36 +0000

    added formFields.js component

---
+++ react-app/src/components/formFields.js
@@ -0,0 +1,75 @@
+const loginFields = [
+    {
+        labelText : "Email address",
+        labelFor : "email-address",
+        id : "email-address",
+        name : "email",
+        type : "email",
+        autocomplete : "email",
+        isRequired : true,
+        placeholder : "Email address"
+    },
+    {
+        labelText : "Password",
+        labelFor : "password",
+        id : "password",
+        name : "password",
+        type : "password",
+        autocomplete : "current-password",
+        isRequired : true,
+        placeholder : "Password"
+    }
+]
+
+const signupFields = [
+    {
+        labelText : "First Name",
+        labelFor : "first-name",
+        id : "first-name",
+        name : "first-name",
+        type : "text",
+        autocomplete : "first-name",
+        isRequired : true,
+        placeholder : "First Name"
+    },
+    {
+        labelText : "Email address",
+        labelFor : "email-address",
+        id : "email-address",
+        name : "email",
+        type : "email",
+        autocomplete : "email",
+        isRequired : true,
+        placeholder : "Email address" 
+    },
+    {
+        labelText : "Last Name",
+        labelFor : "last-name",
+        id : "last-name",
+        name : "last-name",
+        type : "text",
+        autocomplete : "last-name",
+        isRequired : true,
+        placeholder : "Last Name"
+    },
+    {
+        labelText : "Password",
+        labelFor : "password",
+        id : "password",
+        name : "password",
+        type : "password",
+        autocomplete : "current-password",
+        isRequired : true,
+        placeholder : "Password"
+    },
+    {
+        labelText : "Confirm password",
+        labelFor : "confirm-password",
+        id : "confirm-password",
+        name : "confirm-password",
+        type : "password",
+        autocomplete : "confirm-password",
+        isRequired : true,
+        placeholder : "Confirm password"
+    },
+]
