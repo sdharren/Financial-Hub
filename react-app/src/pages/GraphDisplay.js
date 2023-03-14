bf66ecea246d55affc45435e313bb5342d4dad9b
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2023-03-14 17:50:34 +0000

    Add tabs to GraphDisplay.js

--- react-app/src/pages/GraphDisplay.js
+++ react-app/src/pages/GraphDisplay.js
@@ -7,6 +7,10 @@ function GraphDisplay() {
     const [graph, setGraph] = useState(<PieChart endpoint={"investment_categories"} loadNext={handleLoadNext}/>);
     let {authTokens, logoutUser} = useContext(AuthContext);
 
+    const [overviewActive, setOverviewActive] = useState(true);
+    const [categoryActive, setCategoryActive] = useState(false);
+    const [stocksActive, setStocksActive] = useState(false);
+
     let link_sandbox = async() => {
         let response = await fetch('http://127.0.0.1:8000/api/sandbox_investments/',
             {
@@ -33,22 +37,39 @@ function GraphDisplay() {
     // passed as a parameter to the pie chart to update this page once a section of the pie chart is clicked
     function handleLoadNext(event) {
         let next = nextRoute[event.current];
+
         if (next == 'stock_history') {
+            setCategoryActive(false);
+            setStocksActive(true);
+            
             setGraph(
                 <LineGraph endpoint={next} endpoint_parameter={event.next} />
             );
         }
         else {
+            let nextEndpoint = nextRoute[event.current];
+            if (nextEndpoint === 'investment_category_breakdown') {
+                setOverviewActive(false);
+                setCategoryActive(true);
+            }
             setGraph(
-                <PieChart endpoint={nextRoute[event.current]} endpoint_parameter={event.next} loadNext={handleLoadNext} />
+                <PieChart endpoint={nextEndpoint} endpoint_parameter={event.next} loadNext={handleLoadNext} />
             );
         }
     }
 
 
     return (
-        <div style={{width: '45rem', margin: 'auto', padding: '2rem'}} onLoad={link_sandbox()}>
-            {graph}
+        <div>
+            <div class="tab">
+                <button className={"tablinks" + (overviewActive ? " active" : "") } onclick="openCity(event, 'London')">Overview</button>
+                <button className={"tablinks" + (categoryActive ? " active" : "") } onclick="openCity(event, 'Paris')">Category</button>
+                <button className={"tablinks" + (stocksActive ? " active" : "") } onclick="openCity(event, 'Tokyo')">Stocks?</button>
+            </div>
+
+            <div className="tabcontent" onLoad={link_sandbox()}>
+                {graph}
+            </div>
         </div>
     );
 }
