2295b2547baae6850a23b50887ff1dcd501d9423
sdharren <sdharren@yahoo.co.uk>
2023-03-16 03:48:03 +0000

    changed list options of navbar when logged in

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -4,10 +4,9 @@ import AuthContext from '../context/AuthContext';
 
 export default function Navbar() {
     let {user, logoutUser} = useContext(AuthContext);
-    return <nav className="nav">
-        <Link to = "/" className="site-title">Financial Hub</Link>
+
+    let defaultForm = (
         <ul>
-            {user && <li><p>Hello, {user.email}</p></li>}
             <li>
                 <Link to="/about">About</Link>
             </li>
@@ -15,8 +14,39 @@ export default function Navbar() {
                 <Link to="/signup">Sign Up</Link>
             </li>
             <li>
-                {user ? (<p onClick = {logoutUser}>Logout</p>): (<Link to ="/login">Login</Link>)}
+                <Link to ="/login">Login</Link>
             </li>
         </ul>
-    </nav>
+    );
+
+    let loggedInForm = (
+        <ul>
+            <li>
+                {user && <p>Hello, {user.email}</p>}
+            </li>
+            <li>
+                <p onClick = {logoutUser}>Logout</p>
+            </li>
+        </ul>
+    );
+
+    return (
+        <nav className="nav">
+            <Link to = "/" className="site-title">Financial Hub</Link>
+            {/* <ul>
+                {user && <li><p>Hello, {user.email}</p></li>}
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/signup">Sign Up</Link>
+                </li>
+                <li>
+                    {user ? (<p className='logout' onClick = {logoutUser}>Logout</p>): (<Link to ="/login">Login</Link>)}
+                </li>
+            </ul> */}
+            {user ? loggedInForm : defaultForm}
+            {/* {defaultForm} */}
+        </nav>
+    )
 }
