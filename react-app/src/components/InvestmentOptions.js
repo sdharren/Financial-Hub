389f9fdfc4140c04215d9c3104d42f9a07c80d19
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2023-03-16 14:37:52 +0000

    Continue working on InvestmentOptions.js

--- react-app/src/components/InvestmentOptions.js
+++ react-app/src/components/InvestmentOptions.js
@@ -1,32 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import AuthContext from '../context/AuthContext';
 
 function InvestmentOptions ({ options, handleSelectionUpdate, selectedOption, optionType }) {
+    let {authTokens, logoutUser} = useContext(AuthContext);
     const [selected, setSelected] = useState(selectedOption);
     const [selectOptions, setSelectOptions] = useState(
-        options.map(o => (
-            <option key={o} value={o}>{o}</option>
-        ))
+        null
     );
 
-    // useEffect((selected) => {
-    //     setSelectOptions(
-    //         options.map(o => (
-    //             o === selected ? 
-    //             <option key={o} value={o} selected={true}>{o}</option> :
-    //             <option key={o} value={o}>{o}</option>
-    //         ))
-    //     );
-    // }, [selected])
+    let get_options = async() => {
+        
+        if (optionType === 'investment_categories') {
+            let response = await fetch('http://127.0.0.1:8000/api/investment_category_names/',
+                {
+                    method:'GET',
+                    headers:{
+                        'Content-Type':'application/json',
+                        'Authorization':'Bearer ' + String(authTokens.access)
+                    }
+                }
+            );
+            let data = await response.json();
+            console.log(data)
+            setSelectOptions(
+                data['categories'].map(o => (
+                    <option key={o} value={o}>{o}</option>
+                ))
+            )
+        }
+        else if (optionType === 'investment_category_breakdown') {
+            let response = await fetch('http://127.0.0.1:8000/api/supported_investments/',
+                {
+                    method:'GET',
+                    headers:{
+                        'Content-Type':'application/json',
+                        'Authorization':'Bearer ' + String(authTokens.access)
+                    }
+                }
+            );
+            let data = await response.json();
+            setSelectOptions(
+                data['investments'].map(o => (
+                    <option key={o} value={o}>{o}</option>
+                ))
+            )
+        }
+    }
+
+    useEffect(() => {
+        get_options();
+    }, [optionType]);
 
     function handleClick(select) {
         setSelected(select);
-        setSelectOptions(
-            options.map(o => (
-                o === select ? 
-                <option key={o} value={o} selected={true}>{o}</option> :
-                <option key={o} value={o}>{o}</option>
-            ))
-        );
+        // setSelectOptions(
+        //     options.map(o => (
+        //         o === select ? 
+        //         <option key={o} value={o} selected={true}>{o}</option> :
+        //         <option key={o} value={o}>{o}</option>
+        //     ))
+        // );
         handleSelectionUpdate({
             'optionType': optionType,
             'nextSelect': select
