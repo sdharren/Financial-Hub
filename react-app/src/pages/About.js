0663235f934dc14d406d0565ddd27d2b2c346469
sdharren <sdharren@yahoo.co.uk>
2023-03-23 20:15:31 +0000

    change font sizes and added flex boxes to about

--- react-app/src/pages/About.js
+++ react-app/src/pages/About.js
@@ -4,10 +4,9 @@ import etherium from '../images/about-crypto.png';
 import buy from '../images/about-stock.png';
 
 export default function About() {
-    return (
+
+    let page1 = (
         <div>
-            
-        
             <div className="about-page">
             <h1 className='txt-about-us'>About Us</h1>
             <p className='txt-monitoring-finances'>Your go-to app for monitoring finances</p>
@@ -45,4 +44,30 @@ export default function About() {
             </div>
         </div>
   );
+    
+    let page2 = (
+        <div className='about-container flex flex-col my-10 mx-20 gap-24'>
+            <div className='about-text-box flex flex-col gap-8'>
+                <div className='about-us text-white/75 text-center text-4xl'>About Us</div>
+                <div className='about-caption text-white text-center text-5xl font-bold'>Your go-to app for monitoring your finances</div>
+                <div className='link-message text-white text-center text-2xl font-light'>Just link any of your assets to the dashboard and let us do our magic ✨</div>
+            </div>
+            <div className='about-us-cards flex flex-row justify-center gap-3 items-stretch'>
+                <div className='crypto-card flex flex-col gap-2'>
+                    <div className='crypto-label text-white text-center text-3xl font-medium'>Crypto</div>
+                    <img className='rounded-3xl shadow-lg bg-gradient-to-r from-violet-500 to-violet-600' src={etherium} alt="Crypto" /> 
+                </div>
+                <div className='bank-card flex flex-col'>
+                    <div className='bank-label text-white text-center text-3xl font-medium'>Bank</div>
+                    <img className='rounded-3xl shadow-lg bg-gradient-to-r from-violet-500 to-violet-600' src={ecommerce} alt="Banks" /> 
+                </div>
+                <div className='stocks-card flex flex-col'>
+                    <div className='stocks-label text-white text-center text-3xl font-medium'>Stocks</div>
+                    <img className='rounded-3xl shadow-lg bg-gradient-to-r from-violet-500 to-violet-600' src={buy} alt="Stocks" /> 
+                </div>
+            </div>
+        </div>
+    );
+
+    return page2
 }
--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -42,11 +42,11 @@ export default function Home() {
     let page2 = (
         <div className='home-container flex flex-col my-10px mx-20 p-10 gap-8'>
             <div className='welcome-box flex flex-row rounded-3xl shadow-lg bg-gradient-to-r from-violet-500 to-violet-600 py-5 px-10'>
-                <p className='welcome-box-text my-auto align-middle text-4xl text-white'>Welcome to the future of financial monitoring - track everything from bank cards, stocks, and crypto.</p>
+                <p className='welcome-box-text my-auto text-4xl text-white'>Welcome to the future of financial monitoring - track everything from bank cards, stocks, and crypto.</p>
                 <img class="welcome-box-image" src={cards} alt="#" />
             </div>
             <div className='aggregate-box flex flex-row rounded-3xl shadow-lg bg-gradient-to-r from-violet-500 to-violet-600 py-5 px-10'>
-                <p class="aggregate-box-text my-auto align-middle text-4xl text-white">Our aggregate asset tracking and dynamic charts will provide you with the best understanding of your finances.</p>
+                <p class="aggregate-box-text my-auto text-4xl text-white">Our aggregate asset tracking and dynamic charts will provide you with the best understanding of your finances.</p>
                 <img class="home__first__image" src={computer} alt="#"></img>
             </div>
 
