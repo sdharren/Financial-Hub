75c4e1d6268eb2cf3ae0f9a9d5b2ac44d48f4c74
Guilherme Rio-Tinto <k21087441@kcl.ac.uk>
2023-03-23 10:28:35 +0000

    Complete refactoring of component logic

--- react-app/src/requests/RequestOptions.js
+++ react-app/src/requests/RequestOptions.js
@@ -1,6 +1,6 @@
 export const BASE_URL = "http://127.0.0.1:8000/api/";
 
-const createUrl = (base, path, auth) => `${base}${path}`;
+const createUrl = (base, path) => `${base}${path}`;
 
 export const getData = ({endpoint, endpoint_parameter, authTokens}) => [
     createUrl(BASE_URL, (String(endpoint) + (endpoint_parameter != null ? '?param='+endpoint_parameter : '/'))),
