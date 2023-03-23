4f587a62063ccf840030b4b5d31245801820bbcc
Abhishek Rao Chimbili <abhishekraochimbili@gmail.com>
2023-03-23 12:31:40 +0000

    Redesigned the crypto wallet page

--- react-app/src/pages/CryptoWalletAddresses.js
+++ react-app/src/pages/CryptoWalletAddresses.js
@@ -1,54 +1,64 @@
-import React, { useContext } from "react";
-import { useState } from "react";
+import { useState } from 'react';
 
-const SaveWalletAddresses = () => {
-    const [options, setOptions] = useState([
-      { id: 1, value: '' },
-    ]);
-  
-    const handleAddOption = () => {
-      const newOptionId = options.length + 1;
-      setOptions([...options, { id: newOptionId, value: '' }]);
-    };
-  
-    const handleRemoveOption = (id) => {
-      const newOptions = options.filter((option) => option.id !== id);
-      setOptions(newOptions);
-    };
-  
-    const handleOptionChange = (id, value) => {
-      const updatedOptions = options.map((option) => {
-        if (option.id === id) {
-          return { ...option, value };
-        }
-        return option;
-      });
-      setOptions(updatedOptions);
-    };
+function CryptoWalletAddresses() {
+  const [addresses, setAddresses] = useState([{address: '', type: 'btc'}]);
 
-    let form0 = (
-        <div class="address-form">
-        {options.map((option) => (
-          <div class="address-form-content" key={option.id}>
-            <label htmlFor={`option-${option.id}`}>{`Wallet Address ${option.id}:`}</label>
-            <input type="text" id={`option-${option.id}`} name={`option-${option.id}`} value={option.value} onChange={(e) => handleOptionChange(option.id, e.target.value)} />
-            
-            <div class="inline-wallet-address-container">
-                <label htmlFor="multiple-choice-1">Bitcoin: </label>
-                <input type="radio" id="btc-choice-1" name="multiple-choice" />    
-                <label htmlFor="multiple-choice-1">Ethereum: </label>
-                <input type="radio" id="eth-choice-1" name="multiple-choice" />
-            </div>
-
-            <button type="button" onClick={() => handleRemoveOption(option.id)}>Remove</button>
-            <button type="button" onClick={handleAddOption}>Add Address</button>
-          </div>
-        ))}
-      </div>
-    )
-  
-    return (form0);
+  function handleAddressChange(index, key, value) {
+    const newAddresses = [...addresses];
+    newAddresses[index][key] = value;
+    setAddresses(newAddresses);
   }
 
-export default SaveWalletAddresses
+  function handleAddAddress() {
+    setAddresses([...addresses, {address: '', type: 'btc'}]);
+  }
 
+  function handleRemoveAddress(index) {
+    const newAddresses = [...addresses];
+    newAddresses.splice(index, 1);
+    setAddresses(newAddresses);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Handle submission of addresses here
+    const allAddresses = [...addresses];
+    
+  }
+
+  return (
+    <form className="crypto-wallet-addresses" onSubmit={handleSubmit}>
+      {addresses.map((address, index) => (
+        <div key={index}>
+          <label>
+            Address {index + 1}:
+            <input
+              type="text"
+              value={address.address}
+              onChange={(event) => handleAddressChange(index, 'address', event.target.value)}
+            />
+          </label>
+          <label>
+            Type:
+            <select
+              value={address.type}
+              onChange={(event) => handleAddressChange(index, 'type', event.target.value)}
+            >
+              <option value="btc">Bitcoin</option>
+              <option value="eth">Ethereum</option>
+            </select>
+          </label>
+          <button type="button" className="remove-button" onClick={() => handleRemoveAddress(index)}>
+            Remove
+          </button>
+        </div>
+      ))}
+      <button type="button" className="add-button" onClick={handleAddAddress}>
+        Add Address
+      </button>
+      <button type="submit" className="submit-button">Submit</button>
+    </form>
+  );
+}
+
+export default CryptoWalletAddresses
