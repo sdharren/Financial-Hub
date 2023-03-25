ab984c0b64ec8d02adafd12f31a5982a741affde
AUGUSTO FAVERO <augusto.favero@kcl.ac.uk>
2023-03-25 17:03:01 +0000

    Add seeder and unseed commands with tests

---
+++ assetManager/management/commands/seed.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from assetManager.models import User,AccountType,AccountTypeEnum
+from assetManager.API_wrappers import plaid_wrapper,crypto_wrapper,yfinance_wrapper
+from assetManager.API_wrappers.sandbox_wrapper import SandboxWrapper
+
+class Command(BaseCommand):
+    def __init__(self):
+        super().__init__()
+
+    #seed the database with custom user to access the application
+    def handle(self, *args, **options):
+        user = User.objects.create_user(
+        email = 'johndoe@example.org',
+        first_name = 'Johnny',
+        last_name = 'Doe',
+        password = 'Password123',
+        )
+
+        wrapper = SandboxWrapper()
+        public_token = wrapper.create_public_token(bank_id='ins_115616', products_chosen=['investments','transactions'])
+        wrapper.exchange_public_token(public_token)
+        wrapper.save_access_token(user, ['investments','transactions'])
+
+        second_public_token = wrapper.create_public_token(bank_id='ins_12', products_chosen=['investments','transactions'])
+        wrapper.exchange_public_token(second_public_token)
+        wrapper.save_access_token(user, ['transactions','investments'])
---
+++ assetManager/tests/commands/test_seed_and_unseed_command.py
@@ -0,0 +1,58 @@
+from django.core.management import call_command
+from django.test import TestCase
+from assetManager.models import User,AccountType,AccountTypeEnum
+class SeedAndUnseedCommandTestCase(TestCase):
+
+    def test_seed_command(self):
+        accounttypes = AccountType.objects.all()
+        users = User.objects.all()
+
+        before_account_types_count = AccountType.objects.count()
+        before_user_count = User.objects.count()
+
+        for account_type in accounttypes:
+            self.assertTrue(account_type.user.email != 'johndoe@example.org')
+
+        for single_user in users:
+            self.assertTrue(single_user.email != 'johndoe@example.org')
+
+        call_command('seed')
+
+        after_account_types_count = AccountType.objects.count()
+        after_user_count = User.objects.count()
+
+        self.assertEqual(before_user_count + 1, after_user_count)
+        self.assertEqual(before_account_types_count + 4, after_account_types_count)
+
+        user_john = User.objects.get(email = 'johndoe@example.org')
+
+        bank_assets = AccountType.objects.filter(user = user_john, account_asset_type = AccountTypeEnum.DEBIT)
+        investment_assets = AccountType.objects.filter(user = user_john, account_asset_type = AccountTypeEnum.STOCK)
+
+        for bank in bank_assets:
+            self.assertTrue(bank.account_institution_name == 'Vanguard' or bank.account_institution_name == 'Fidelity')
+
+        for investment in investment_assets:
+            self.assertTrue(investment.account_institution_name == 'Vanguard' or investment.account_institution_name == 'Fidelity')
+
+    def test_unseed_command(self):
+        call_command('seed')
+        before_account_types_count = AccountType.objects.count()
+        before_user_count = User.objects.count()
+        call_command('unseed')
+        after_account_types_count = AccountType.objects.count()
+        after_user_count = User.objects.count()
+        self.assertEqual(after_user_count, before_user_count - 1)
+        self.assertEqual(after_account_types_count, before_account_types_count - 4)
+        
+        accounttypes = AccountType.objects.all()
+        users = User.objects.all()
+
+        before_account_types_count = AccountType.objects.count()
+        before_user_count = User.objects.count()
+
+        for account_type in accounttypes:
+            self.assertTrue(account_type.user.email != 'johndoe@example.org')
+
+        for single_user in users:
+            self.assertTrue(single_user.email != 'johndoe@example.org')
