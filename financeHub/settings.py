c41d547d6004fe10c958b874a187a02b9165bceb
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2023-03-25 20:40:58 +0000

    Add media file dir to settings

--- financeHub/settings.py
+++ financeHub/settings.py
@@ -180,6 +180,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -188,6 +189,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
