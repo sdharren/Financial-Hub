86ff897568a0a3b3ea186a2de0f46cc4951d73a8
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2023-03-25 20:11:44 +0000

    Change staticfiles dirs setting

--- financeHub/settings.py
+++ financeHub/settings.py
@@ -184,7 +184,7 @@ STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
