e705caadc33b32843cc052a83d1774e9a4cd1328
Abhishek Rao Chimbili <abhishekraochimbili@gmail.com>
2023-03-26 21:41:20 +0100

    Create test_crypto_wrapper.py

---
+++ assetManager/tests/API_wrappers/test_crypto_wrapper.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from assetManager.API_wrappers.crypto_wrapper import getAddressData, getUsable, getAllData
+
+# Run Tests using a static address with fixed attributes
+# BTC = 
+# ETH = 
+
+class CryptoWrapperTestCase(TestCase):
+    def test_get_address_data(self):
+
+        return True
