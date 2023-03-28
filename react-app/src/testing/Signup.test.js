1a26249cc9811a5f5fcf2307f7d2a47d88a0db77
sdharren <sdharren@yahoo.co.uk>
2023-03-28 03:43:25 +0100

    signup tests

--- react-app/src/testing/Signup.test.js
+++ react-app/src/testing/Signup.test.js
@@ -1,17 +1,87 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Signup from "../pages/Signup";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "../context/AuthContext";
+import customRender from "./test-utils";
+import userEvent from "@testing-library/user-event";
 
 describe("Signup component", () => {
     it("renders without crashing", () => {
-        render(
-            <BrowserRouter>
-                <AuthProvider>
-                    <Signup />
-                </AuthProvider>
-            </BrowserRouter>
+        customRender(
+            <Signup />
         )
+    })
+
+    it("has first and last name, email, password and password confirmation fields", () => {
+        customRender(
+            <Signup />
+        )
+
+        const firstName = screen.getByLabelText(/First Name/i)
+        const lastName = screen.getByLabelText(/Last Name/i)
+        const password = screen.getByLabelText('Password')
+        const email = screen.getByLabelText(/Email address/i)
+        const passconf = screen.getByLabelText(/Confirm password/i)
+
+        expect(firstName).toBeInTheDocument()
+        expect(lastName).toBeInTheDocument()
+        expect(email).toBeInTheDocument()
+        expect(password).toBeInTheDocument()
+        expect(passconf).toBeInTheDocument()
+    })
+
+    it("email field is of type 'email'", () => {
+        customRender(
+            <Signup />
+        )
+
+        const email_field = screen.getByLabelText(/Email address/i)
+        expect(email_field).toHaveProperty("type", "email")
+    })
+
+    it("password field is of type 'password'", () => {
+        customRender(
+            <Signup />
+        )
+
+        const password_field = screen.getByLabelText('Password')
+        expect(password_field).toHaveProperty("type", "password")
+    })
+
+    it("password confirmation field is of type 'password'", () => {
+        customRender(
+            <Signup />
+        )
+
+        const password_conf_field = screen.getByLabelText(/Confirm Password/i)
+        expect(password_conf_field).toHaveProperty("type", "password")
+    })
+
+    it("has a register button", () => {
+        customRender(
+            <Signup />
+        )
+
+        const registerButton = screen.getByText('Register')
+        expect(registerButton).toBeInTheDocument()
+    })
+
+    it("register is of type 'submit'", () => {
+        customRender(
+            <Signup />
+        )
+
+        const registerButton = screen.getByText('Register')
+        expect(registerButton).toHaveProperty('type', 'submit')
+    })
+
+    it("register button calls function on click", () => {
+        const testfn = jest.fn()
+        customRender(
+            <Signup submit={testfn}/>
+        )
+
+        const registerButton = screen.getByText('Register')
+        userEvent.click(registerButton)
+        expect(testfn).toBeCalled()
     })
 })
