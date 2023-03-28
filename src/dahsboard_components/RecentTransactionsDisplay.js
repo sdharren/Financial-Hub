ba9df5317dd63603bd9414252317ec130e81efab
sdharren <sdharren@yahoo.co.uk>
2023-03-28 21:44:33 +0100

    bold font

--- src/dahsboard_components/RecentTransactionsDisplay.js
+++ src/dahsboard_components/RecentTransactionsDisplay.js
@@ -66,7 +66,7 @@ function RecentTransactions() {
           {categories.map((category) => (
             <TableCell 
               key={category}
-              style={{color: 'white', fontWeight: 'extrabold', fontSize: '1.5rem'}}
+              style={{color: 'white', fontWeight: 'bold', fontSize: '1.5rem'}}
             >
               {category}
             </TableCell>
